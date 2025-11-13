e2cab1463bf5ebffa1233a796c15a59e07a4e00a
Iti Karmakar <itikarmakar2004@gmail.com>
2025-11-13 18:44:21 +0530

    Made few changes

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://full-stack-blog-website-aurora-blog.vercel.app/api',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json'
