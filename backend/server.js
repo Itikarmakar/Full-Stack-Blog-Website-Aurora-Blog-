c76f28ac4177541e408218cc1f5117e03703443e
Iti Karmakar <itikarmakar2004@gmail.com>
2025-11-13 16:49:12 +0530

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,10 @@ dotenv.config();
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello from Express on Vercel!' });
+});
+
 // Connect to database
 connectDB();
 
@@ -85,4 +89,5 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+export default app;
 
