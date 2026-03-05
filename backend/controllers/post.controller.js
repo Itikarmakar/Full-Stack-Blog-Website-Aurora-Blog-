96a1ce3fac288e4227e0115e64e2e8323c733577
Iti Karmakar <itikarmakar2004@gmail.com>
2026-03-05 20:53:41 +0530

    Enhance post retrieval and creation: increment view count on single post access, add reading time calculation, and log content processing steps

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -1,5 +1,6 @@
 import Post from '../models/Post.js';
 
+
 const allPosts = async(req,res) => {
     try {
     const posts = await Post.find().sort({ createdAt: -1 });
@@ -11,7 +12,11 @@ const allPosts = async(req,res) => {
 
 const singlePost = async(req,res) => {
     try {
-    const post = await Post.findById(req.params.id);
+    const post = await Post.findByIdAndUpdate(
+      req.params.id,
+      { $inc: { views: 1 } }, // increase views by 1
+      { new: true } // return updated post
+    );
     if (!post) {
       return res.status(404).json({ message: 'Post not found' });
     }
@@ -22,20 +27,39 @@ const singlePost = async(req,res) => {
 }
 
 const createPost = async(req,res) => {
+  console.log("triggered")
     try {
     const { title, author, content, image } = req.body;
+
+    console.log("Content received:", content);
 
     if (!title || !author || !content) {
       return res.status(400).json({ message: 'Please provide all required fields' });
     }
+
+    // Remove HTML tags if content comes from TinyMCE
+    const plainText = content.replace(/<[^>]*>/g, "");
+
+    console.log("Plain text:", plainText);
+
+    // Calculate reading time
+    const wordsPerMinute = 200;
+    const wordCount = plainText.trim().split(/\s+/).length;
+    const readingTime = Math.ceil(wordCount / wordsPerMinute);
+
+    console.log("Reading time:", readingTime);
 
     const post = await Post.create({
       title,
       author,
       authorId: req.user._id,
       content,
-      image: image || ''
+      image: image || '',
+      readingTime,
+      views: 0
     });
+
+    console.log("Saved post:", post);
 
     res.status(201).json(post);
   } catch (error) {
